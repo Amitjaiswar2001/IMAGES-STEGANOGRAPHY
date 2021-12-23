4131d01f7fe8ff9d498648f676476406d73e522b
Amit Vinod Jaiswar <96528417+Amitjaiswar2001@users.noreply.github.com>
2021-12-23 15:46:10 +0530

    main file of the image steganography
    source code

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Coding;
+
+/**
+ *
+ * @author Amitj
+ */
+public class Main {
+    public static void main(String args[]){
+    MenuFrame mf = new MenuFrame();
+    mf.setVisible(true);
+    mf.setLocationRelativeTo(null);
+}
+}
